b559b1d20f1238294d442c069d9a91a8cb9a11f4
souravk1986 <kundusourav1986@gmail.com>
2024-05-26 12:15:37 -0400

    Update MissingNumber.java
    Adding another solution

--- MissingNumber.java
+++ MissingNumber.java
@@ -57,3 +57,18 @@ class Solution {
     }
 }
 **/
+
+/** Using math
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        int n = nums.length;
+        int continious_sum = (n * (n+1))/2;
+        int sum = 0;
+        for(int i = 0; i< nums.length; i++) {
+            sum += nums[i];
+        }
+        return continious_sum - sum;
+    }
+}
+**/
